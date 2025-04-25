d06c908dc6ca77034fb714786cf164a0d2752755
uitvoerbaar <pdiender@outlook.com>
2025-04-25 08:27:24 +0000

    Add monafit Tracker app setup instructions and code

---
+++ octofit-tracker/frontend/src/components/Activities.js
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from 'react';
+
+const Activities = () => {
+  const [activities, setActivities] = useState([]);
+
+  useEffect(() => {
+    fetch('https://friendly-meme-g4w6p9gvx9j395g6-8000.app.github.dev/api/activities')
+      .then(response => response.json())
+      .then(data => setActivities(data));
+  }, []);
+
+  return (
+    <div className="container mt-4">
+      <h1 className="text-center">Activities</h1>
+      <table className="table table-striped">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+          </tr>
+        </thead>
+        <tbody>
+          {activities.map(activity => (
+            <tr key={activity.id}>
+              <td>{activity.id}</td>
+              <td>{activity.name}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default Activities;
