adbff50a79da67402bd533e7e3fb191ac7b58e6b
uitvoerbaar <pdiender@outlook.com>
2025-04-25 07:53:27 +0000

    Commit changes

--- octofit-tracker/backend/octofit_tracker/views.py
+++ octofit-tracker/backend/octofit_tracker/views.py
@@ -1,6 +1,19 @@
 from rest_framework import viewsets
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from .serializers import UserSerializer, TeamSerializer, ActivitySerializer, LeaderboardSerializer, WorkoutSerializer
 from .models import User, Team, Activity, Leaderboard, Workout
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    base_url = 'https://friendly-meme-g4w6p9gvx9j395g6-8000.app.github.dev/'
+    return Response({
+        'users': base_url + 'api/users/?format=api',
+        'teams': base_url + 'api/teams/?format=api',
+        'activities': base_url + 'api/activities/?format=api',
+        'leaderboard': base_url + 'api/leaderboard/?format=api',
+        'workouts': base_url + 'api/workouts/?format=api'
+    })
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
